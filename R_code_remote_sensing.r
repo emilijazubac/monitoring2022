266fd840e73e69d3db83c965235f080c202047b0
emilijazubac <114756020+emilijazubac@users.noreply.github.com>
2022-11-10 17:10:48 +0100

    Create R_code_remote_sensing.r

---
+++ R_code_remote_sensing.r
@@ -0,0 +1,28 @@
+ install.packages(c("raster","RStoolbox"))
+library(raster)
+library(RStoolbox)
+#rasterbrick is a file with more layers formed from pixels with the valu of the reflextants
+#grd is the type of file we are using
+#masked means that part of the data was cleaned
+setwd("C:/lab/")
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+
+p224r63_2011
+
+plot(p224r63_2011)
+
+cl <- colorRampPalette(c('black','grey','light grey'))(100) # 
+plot(p224r63_2011, col=cl)
+
+
+par(mfrow=c(2,2))
+
+clb <- colorRampPalette(c('dark blue','blue','light blue'))(100) # 
+plot(p224r63_2011$B1_sre, col=clb)
+
+clg <- colorRampPalette(c('dark green','green','light green'))(100) # 
+plot(p224r63_2011$B2_sre, col=clg)
+
+clr <- colorRampPalette(c('dark red','red','pink'))(100) # 
+plot(p224r63_2011$B3_sre, col=clr)
+
